749658978003a7b837312f69182ab5181a3bcb68
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-04-19 15:15:55 -0500

    nav and about components created

---
+++ src/components/Nav/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Nav() {
+  
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="Matthew"></span> Matthew McElwee
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                  
+                 
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
