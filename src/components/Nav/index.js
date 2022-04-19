e457d38afab9d1f920b3f57d9c31098da4249bd9
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-04-19 18:32:15 -0500

    portfolio componant and css added

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,28 +2,20 @@ import React from 'react';
 
 
 function Nav() {
-  
+
     return (
         <header>
-            <h2>
-                <a href="/">
-                    <span role="img" aria-label="Matthew"></span> Matthew McElwee
-                </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
-                            About me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                  
-                 
-                </ul>
-            </nav>
+            <div class="w3-top">
+                <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+                    <a href="#home" class="w3-bar-item w3-button"> Matthew McElwee</a>
+                    {/* Float links to the right. Hide them on small screens */}
+                    <div class="w3-right w3-hide-small">
+                        <a href="#projects" class="w3-bar-item w3-button">Projects</a>
+                        <a href="#about" class="w3-bar-item w3-button">About</a>
+                        <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import background from "../../assets/images/background.jpg";
+function Portfolio() {
+    return (
+        <section>
+            <img src={background} className="my-2" style={{ width: "100%" }} alt="cover" />
+            {/* <h1>NAME</h1>
+            <p>DESCRIPTION</p> */}
+        </section>
+    );
+}
+export default Portfolio;
